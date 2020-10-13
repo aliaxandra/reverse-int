7e126ac63f3c5af4118f95623c9aced79788ea06
Alexandra <aliaxandra@gmail.com>
2020-10-13 14:57:37 +0300

    init: add reverse-int task and solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
   
+    let str = String(n);
+
+    if (str[0] === '-') {
+        str = str.slice(1);
+    }
+
+    return str.split('').reverse().join('')
 }
